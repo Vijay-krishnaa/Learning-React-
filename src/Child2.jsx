18ed845bb2dcd1ed06afe8c066224d20d7d63305
Vijay Krishna <vkk7739@gmail.com>
2024-10-09 15:45:52 +0530

    useState in f/n based components

--- src/Child2.jsx
+++ src/Child2.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Child2() {
-  return <div></div>;
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <>
+      <button onClick={increment}>Increment</button>
+      <b> {count}</b>
+      <button onClick={decrement}>Decrement</button>
+    </>
+  );
 }
 
 export default Child2;
